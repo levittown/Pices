ee61f6fcfb8bfb67c896e9ef61e90519dec89127
Kurt Kramer <kurtkramer@gmail.com>
2014-12-14 01:20:30 -0500

    jkl

--- PicesDataBase/StoredProcedures/InstrumentData.sql
+++ PicesDataBase/StoredProcedures/InstrumentData.sql
@@ -981,15 +981,15 @@ begin
     set _scanRate = 24950;
   end if;
   
-  if  _chamberWidth < 0.001  then
+  if  _chamberWidth < 0.01  then
     set  _chamberWidth = 0.098;
   end if;
   
   set _secsPerRec = 4096 / _scanRate;
 
-  select   Floor(id.depth / _depthBinSize)                           as BinId,
-           Floor(id.depth / _depthBinSize) * _depthBinSize           as BinDepth,
-           sum(id.FlowRate1 * _secsPerRec * (_chamberWidth * 0.098)  as VolumeSampled
+  select   Floor(id.depth / _depthBinSize)                                     as BinId,
+           Floor(id.depth / _depthBinSize) * _depthBinSize                     as BinDepth,
+           sum(id.FlowRate1 * _secsPerRec * (_chamberWidth / 1000.0) * 0.098)  as VolumeSampled
          from  InstrumentData id
          where id.SipperFileId = _sipperFileId
          group by Floor(id.depth / _depthBinSize)
---
+++ Python/ImportInstrumentDataCropSettings-2.py
@@ -0,0 +1,119 @@
+import mysql.connector
+from mysql.connector import errorcode
+#import grt
+#from grt import executeScript
+
+import  datetime
+
+
+
+def  ToFloat(s):
+  f=0.0
+  try:
+    f=float(s)
+  except  ValueError:
+    f=0.0
+  return f
+
+
+
+#try:
+
+#     db = mysql.connector.Connect(user='root',
+#                                  password="dasani30",
+#                                  host='sipper-db.marine.usf.edu',
+#                                  database='pices_new')
+
+try:
+      db = mysql.connector.Connect(user='root',
+                             password="dasani30",
+                             host='localhost',
+                             database='pices_new')
+
+
+except  mysql.connector.Error as err:
+    if  err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Cound notconect to \"Sipper-d-marine.usf.edu\"")
+    elif  err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exists")
+    else:
+        print(err)
+
+c=db.cursor()
+
+fileNames = [
+             "InstrumentDataCropSettings_B0710.txt",
+             "InstrumentDataCropSettings_BE0412.txt",
+             "InstrumentDataCropSettings_BE0413.txt",
+             "InstrumentDataCropSettings_BE0612.txt",
+             "InstrumentDataCropSettings_BE1112.txt",
+             "InstrumentDataCropSettings_ETP2007.txt",
+             "InstrumentDataCropSettings_ETP2008.txt",
+             "InstrumentDataCropSettings_HRS02.txt",
+             "InstrumentDataCropSettings_WB0111.txt",
+             "InstrumentDataCropSettings_WB0211.txt",
+             "InstrumentDataCropSettings_WB0212.txt",
+             "InstrumentDataCropSettings_WB0213.txt",
+             "InstrumentDataCropSettings_WB0511.txt",
+             "InstrumentDataCropSettings_WB0512.txt",
+             "InstrumentDataCropSettings_WB0611.txt",
+             "InstrumentDataCropSettings_WB0812.txt",
+             "InstrumentDataCropSettings_WB0813.txt",
+             "InstrumentDataCropSettings_WB0814.txt",
+             "InstrumentDataCropSettings_WB0911.txt",
+             "InstrumentDataCropSettings_WB1008.txt",
+             "InstrumentDataCropSettings_WB1012.txt",
+             "InstrumentDataCropSettings_WB1101.txt",
+             "InstrumentDataCropSettings_WB1111.txt",
+             "InstrumentDataCropSettings_WFS2008.txt"
+            ]
+
+
+for  fileName in fileNames:
+   print("FileNanme: " + fileName + "\n")
+
+   fileOpened = False
+   try:
+     instrumentDataDataFile=open("D:\\Users\\kkramer\\DropBox\\Dropbox\\Sipper\\ForAndrew\\InstrumentDataCropSettings\\" + fileName)
+     fileOpened = True
+
+   except:
+     print("Error opening file: " + fileName)
+     fileOpened = False
+
+   if  fileOpened:
+      count = 0
+      for line in instrumentDataDataFile:
+        fields=line.split('\t')
+        if  len(fields)>= 5:
+            sipperFileName=fields[0]
+            scanLine      = int(fields[1])
+            cropLeft      = int(fields[2])
+            cropRight     = int(fields[3])
+            activeColumns = int(fields[4])
+ 
+            sqlStr="call InstrumentDataUpdateCropSettings(" +  sipperFileName        + "," + \
+                                                           repr(scanLine)            + "," + \
+                                                           repr(scanLine + 4090)     + "," + \
+                                                           repr(cropLeft)            + "," + \
+                                                           repr(cropRight)           + "," + \
+                                                           repr(activeColumns)       + \
+                                                         ")"
+            #result = grt.root.wb.sqlEditors[0].executeScript(sqlStr)
+
+            if  (count % 1000) == 0:
+              print(count, sqlStr)
+
+            try:
+              c.execute(sqlStr)
+              db.commit()
+            except  mysql.connector.Error  as err:
+              print(err)
+           
+            count = count + 1
+           
+instrumentDataDataFile.close()
+db.close()
+
+
+
