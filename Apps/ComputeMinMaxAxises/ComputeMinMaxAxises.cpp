17de4cdfc3c663f96bc30c6ae42214edb9ff78b9
Kurt Kramer <kurtkramer@gmail.com>
2017-04-22 13:46:18 -0400

    Compute Min and Max Axises

--- Apps/ComputeMinMaxAxises/ComputeMinMaxAxises.cpp
+++ Apps/ComputeMinMaxAxises/ComputeMinMaxAxises.cpp
@@ -19,6 +19,7 @@ using namespace KKB;
 
 
 #include "DataBase.h"
+#include "InstrumentDataFileManager.h"
 #include "SipperFile.h"
 #include "PicesVariables.h"
 using namespace MLL;
@@ -133,6 +134,14 @@ void   ComputeMinMaxAxises::Main ()
   if (Abort ())
     return;
 
+  const double  pixelsPerScanLineDefault = 3800.0;
+  const double  chamberWidthDefault = 0.096f;
+  const double  mmPerMeter = 1000.0;
+  const double  scanRateDefault = 24950.0;
+  const double  flowRate1Default = 0.5;
+
+
+
   DateTime  runDate = osGetLocalDateTime ();
 
   KKStr reportFileDir = osAddSlash (PicesVariables::ReportDir ()) + "ComputeMinMaxAxises";
@@ -186,17 +195,111 @@ void   ComputeMinMaxAxises::Main ()
     SipperFilePtr sf = *idx;
     sipperFileNum++;
 
-    KKStr  fullSipperFileName = InstrumentDataFileManager::GetFullSipperFileName (sf->SipperFileName ());
-    if (fullSipperFileName.Empty ())
-    {
-      log.Level (-1) << endl << "Sipper File: " << sf->SipperFileName () << "*** Not Found ***" << endl << endl;
-      r << endl << "Sipper File: " << sf->SipperFileName () << "*** Not Found ***" << endl << endl;
-      continue;
+    auto deployment = DB ()->SipperDeploymentLoad (sf->CruiseName (), sf->StationName (), sf->DeploymentNum ());
+
+    auto images = DB ()->ImagesQuery(NULL,     // Group Name,
+                                      sf->SipperFileName(),  //CruiseName (), sf->StationName (), sf->DeploymentNum (),
+                                      NULL,       // Class Pointer
+                                      'P',        //'P' = Use Prediced Class
+                                      0.0f,       // minProb 
+                                      0.0f,       // maxProb
+                                      0,          // minSize 
+                                      0,          // maxSize
+                                      0.0f, 0.0f, 0, -1, false, cancelFlag);
+    for (auto image : *images) {
+      auto id = InstrumentDataFileManager::GetClosestInstrumentData (image->ImageFileName (), cancelFlag, log);
+
+      double  pixelsPerScanLine = pixelsPerScanLineDefault;
+      double  flowRate1 = flowRate1Default;
+      double  scanRate = scanRateDefault;
+      double  chamberWidth = chamberWidthDefault;
+
+      if (deployment) {
+        if (deployment->ChamberWidth () > 0.0)
+          chamberWidth = deployment->ChamberWidth ();
+      }
+
+      if (id) {
+        pixelsPerScanLine = (double)(1 + id->CropRight () - id->CropLeft ());
+        flowRate1 = id->FlowRate1 ();
+      }
+
+      if (sf->ScanRate () > 0.0)
+        scanRate = sf->ScanRate ();
+
+      double mmPerPixelAccrossChamber = chamberWidth * mmPerMeter / pixelsPerScanLine;
+      double mmPerPixelWithFlow = flowRate1  * mmPerMeter / scanRate;
+
+      double flowStretchFactor = mmPerPixelAccrossChamber / mmPerPixelWithFlow;
+
+      auto fullSizeImage = DB ()->ImageFullSizeLoad (image->ImageId ());
+
+      auto normalizedImage = fullSizeImage->StreatchImage ((float)flowStretchFactor, 1.0f);
+
+
+      float  eigenRatio;
+      float  orientationAngle;
+
+      normalizedImage->CalcOrientationAndEigerRatio (eigenRatio, orientationAngle);
+      if ((orientationAngle > TwoPie) || (orientationAngle < -TwoPie))
+      {
+        orientationAngle = 0.0;
+        eigenRatio = 1.0;
+      }
+
+      auto rotatedImage = normalizedImage->Rotate (orientationAngle);
+
+      kkint32 minHeight = 99999999;
+      kkint32 minWidth = 9999999;
+
+      // Find minWidth
+      for (auto row = 0; row < rotatedImage->Height (); ++row) {
+        kkint32 firstCol = -1;
+        kkint32 lastCol = -1;
+        for (auto col = 0; col < rotatedImage->Width (); ++col) {
+          if (rotatedImage->ForegroundPixel (row, col)) {
+            lastCol = col;
+            if (firstCol < 0)  firstCol = col;
+          }
+        }
+        auto rowWidth = 1 + lastCol - firstCol;
+        if ((firstCol > 0) && (rowWidth < minWidth))
+          minWidth = rowWidth;
+      }
+
+
+      // Find minHeight
+      for (auto col = 0; col < rotatedImage->Width (); ++col) {
+        kkint32 firstRow = -1;
+        kkint32 lastRow = -1;
+        for (auto row = 0; row < rotatedImage->Height (); ++row) {
+          if (rotatedImage->ForegroundPixel (row, col)) {
+            lastRow = row;
+            if (firstRow < 0)  firstRow = row;
+          }
+        }
+        auto colHeight = 1 + lastRow - firstRow;
+        if ((firstRow > 0) && (colHeight < minHeight))
+          minHeight = colHeight;
+      }
+
+      auto minHeightMM = minHeight * mmPerPixelAccrossChamber;
+      auto minWidthMM = minWidth * mmPerPixelAccrossChamber;
+
+      cout << image->ImageFileName () << "\t" << minHeightMM << "\t" << minWidthMM << endl;
+
+      delete  rotatedImage;
+      rotatedImage = NULL;
+
+      delete  normalizedImage;
+      normalizedImage = NULL;
+
+      delete fullSizeImage;
+      fullSizeImage = NULL;
     }
 
-    log.Level (10) << " " << sipperFileNum << " of " << numSipperFiles << "  File [" << sf->SipperFileName () << "]" << endl;
-    r << osGetLocalDateTime () << "\t" << sf->SipperFileName () << "\t" << fullSipperFileName << endl;
-    DB ()->InstrumentDataReFreshSipperFile (sf->SipperFileName (), cancelFlag, msgBuff, sizeof (msgBuff));
+    delete images;
+    images = NULL;
   }
 
   r << endl << "Done Processing" << endl;
